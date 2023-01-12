dddee6bf711b9a316737d5993ad72cbfb859213a
DoHyun Moon <P000020>
2023-01-12 05:23:39 +0000

    Init

---
+++ smarttableforjenkins/webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+
+sap.ui.require(["com/sap/demo/smarttableforjenkins/test/integration/AllJourneys"
+], function () {
+	QUnit.config.autostart = false;
+	QUnit.start();
+});
---
+++ smarttableforjenkins/webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"comsapdemo/smarttableforjenkins/test/unit/controller/Main.controller"
+], function () {
+	"use strict";
+});
